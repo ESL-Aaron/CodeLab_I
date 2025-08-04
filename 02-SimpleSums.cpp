4e1d92fa846d105b28a92c77d821c76239187759
ESL-Aaron <2406-120015@esl.ac.uk>
2025-08-04 21:40:50 +0200

    Exercise 2: Simple Sums - Completed

---
+++ 02-SimpleSums.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int X = 8;
+    int Y = 10;
+    int addition = X + Y;
+
+    cout << " The addition of " << X << " and " << Y << " is: " << addition << endl;
+    return 0;
+}
