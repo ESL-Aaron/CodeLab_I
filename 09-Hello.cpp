97fab762001ffcc9b7467f5f11ea97c1668865ef
ESL-Aaron <2406-120015@esl.ac.uk>
2025-08-04 21:48:19 +0200

    Exercise 9: Hello - Completed

---
+++ 09-Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void hello(){ // Function to print "Hello", we use void return type because we don't need to return any value.
+  cout << "Hello" << endl;
+}
+
+int main(){
+
+  hello();
+
+  return 0;
+}
